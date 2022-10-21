8258a05f73b036c1c14f5c1255e66d0caa687f44
GURU <gurupdas11@gmail.com>
2022-10-21 21:34:28 +0530

    My selenium Code 2nd commit

---
+++ src/main/java/SeleniumProgram/Selenium/MUNU.java
@@ -0,0 +1,10 @@
+package SeleniumProgram.Selenium;
+
+public class MUNU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("dsbcaisfbcsiaufasofoasnoasnasibciasdciasndo");
+	}
+
+}
